2a7fdff495d6ee0f844664efa4f1cf6c145954e6
abhishek98s <abhishekshakya98.com@gmail.com>
2024-08-30 18:31:47 +0545

    feast: get the user feed

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,9 +1,13 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
 
 import styles from './navbar.module.scss';
+import { useAppSelector } from "@/lib/hooks";
 
 export default function Navbar() {
+    const auth_user = useAppSelector((state) => state.auth.user);
     return (
         <nav className={`${styles.navbar} + fixed z-10 left-0 right-0 top-0 navbar flex justify-between items-center p-[16px_12px] bg-neutral-90 shadow-soft`}>
             <div>
@@ -14,8 +18,8 @@ export default function Navbar() {
 
             <ul>
                 <li className="icon-box-40 bg-neutral-86 rounded-full">
-                    <Link href="/profile/2/feed" className={`${styles.user_profile} icon-box-40 flex-center block rounded-full transition-4`}>
-                        <Image src='/icons/profile.svg' alt="icon-user" width={0} height={0} className="rounded-full max-w-[32px] w-full"/>
+                    <Link href={`/profile/${auth_user?.id!}/feed`} className={`${styles.user_profile} icon-box-40 flex-center block rounded-full transition-4`}>
+                        <Image src={(auth_user?.image!) ? auth_user?.image! : '/icons/profile.svg'} alt="icon-user" width={0} height={0} className="rounded-full max-w-[32px] w-full" />
                     </Link>
                 </li>
             </ul>
