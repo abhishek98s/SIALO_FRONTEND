4a38af94ca6fbaf2440e3a1102887270c491c549
abhishek98s <abhishekshakya98.com@gmail.com>
2024-09-01 12:36:02 +0545

    feat: user feed, friends, photos

--- src/components/image_list-preview/index.tsx
+++ src/components/image_list-preview/index.tsx
@@ -1,27 +1,36 @@
+import { IFeed } from "@/types/home.types.";
+import { APP_BASE_URL } from "@/utils/app";
+import { axiosInterceptor } from "@/utils/axois.config";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function ImageListPreview() {
+    const [randomPhoto, setRandomPhoto] = useState<IFeed[]>([]);
+
+    const axiosInstance = axiosInterceptor();
+
+    const getPhoto = async () => {
+        const response = await axiosInstance.get(`${APP_BASE_URL}/post/random?=noOfPosts=4`);
+        const { status, data } = response.data;
+
+        setRandomPhoto(data);
+    }
+
+    useEffect(() => {
+        getPhoto();
+    }, [])
     return (
-        <div className="image-wrapper hidden lg:block max-w-[240px] w-full rounded-4 border-neutral-96 bg-neutral-90 p-[12px] pt-[20px] h-fit">
+        <div className="image-wrapper hidden lg:block max-w-[240px] w-full border-neutral-80 rounded-4 border-neutral-96 bg-neutral-90 p-[12px] pt-[20px] h-fit">
             <h4 className="heading-line font-bold text-[14px] color-primary-60 mb-[32px]">Photos</h4>
 
             <div className="image-list flex flex-wrap gap-[8px] mb-[8px]">
-                <figure className="rounded-4 border-neutral-86 max-w-[100px] w-full h-[100px] relative">
-                    <Image className="w-full h-auto" src={`/story-2.png`} priority fill={true} alt="user" />
-                </figure>
 
-                <figure className="rounded-4 border-neutral-86 max-w-[100px] w-full h-[100px] relative">
-                    <Image className="w-full h-auto" src={`/story-2.png`} priority fill={true} alt="user" />
-                </figure>
-
-                <figure className="rounded-4 border-neutral-86 max-w-[100px] w-full h-[100px] relative">
-                    <Image className="w-full h-auto" src={`/story-2.png`} priority fill={true} alt="user" />
-                </figure>
-
-                <figure className="rounded-4 border-neutral-86 max-w-[100px] w-full h-[100px] relative">
-                    <Image className="w-full h-auto" src={`/story-2.png`} priority fill={true} alt="user" />
-                </figure>
+                {randomPhoto.map((item, index) => (
+                    <figure key={index} className="rounded-4 border-neutral-86 max-w-[100px] w-full h-[100px] relative">
+                        <Image className="w-full h-auto object-cover rounded-4 border-neutral-80" src={item.post_image} priority fill={true} alt="user" />
+                    </figure>
+                ))}
             </div>
 
             <Link href={`/profile/2/photos`} className="text-center txt-focus block text-[14px] mx-auto color-primary-60">view more</Link>
